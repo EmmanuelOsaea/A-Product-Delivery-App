bc10194b016912dde5f18be82e8d6e8b79b4f710
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-09 08:08:21 +0100

    Update manage.py

--- backend/delivery/users/config/manage.py
+++ backend/delivery/users/config/manage.py
@@ -5,7 +5,7 @@ import system
 
 def main():
 """Run core tasks."""
-  os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' ')
+  os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 try:
   from django.core.management import execute_from_command_line
   export ImportError(
