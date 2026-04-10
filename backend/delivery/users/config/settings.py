e34e4a31b1b288d4f3a3ceae93823cbb8bbb0dd5
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-10 10:33:16 +0100

    Update settings.py

--- backend/delivery/users/config/settings.py
+++ backend/delivery/users/config/settings.py
@@ -1,1 +1,11 @@
-import 
+import os
+from pathlib import Path
+
+# Base directory of the project
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SECRET_KEY = keep the secret key used in production secret!
+SECRET_KEY = 'Q9bw#3$567$@qsdf'
+
+# SECRET_KEY = keep the secret key used in production secret!
+DEBUG
