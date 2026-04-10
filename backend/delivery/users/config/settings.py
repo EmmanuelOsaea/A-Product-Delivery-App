15d8fd1037b588ad423d989fd7e05d130a5a2242
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-10 13:09:07 +0100

    Update settings.py

--- backend/delivery/users/config/settings.py
+++ backend/delivery/users/config/settings.py
@@ -33,3 +33,22 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+   {
+      'BACKEND': 'django.template.backends.django.DjangoTemplates',
+      'DIRS': [BASE_DIR/' ']
+      'APP_DIRS': True,
+      'OPTION': {
+         'context_processors': [
+            'django.template.context_processors.debug',
+             'django.template.context_processors.request',
+             'django.contrib.auth.context_processors.auth',
+             'django.contrib.messages.context_processors.messages',
+         ],
+         },
+         },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+          
