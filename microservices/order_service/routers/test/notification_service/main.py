afacc12fbd534c9f08afa0b6ca8d711d3b741830
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-13 22:01:37 +0100

    Update main.py

--- microservices/order_service/routers/test/notification_service/main.py
+++ microservices/order_service/routers/test/notification_service/main.py
@@ -1,1 +1,16 @@
+from fastapi import FastApi, HTTPException, BackgroundTasks
+from pydantic import BaseModel, EmailStr
+from typing import List
+from pydantic import BaseModel, validator
+from typing import Optional
 
+app = FastAPI(title="Notification Service")
+
+# --- Schemas ---
+class Notification(BaseModel):
+  user_id: int
+  message: str
+  email: Optional[EmailStr] = None
+  discord_id: Optional[str] = None
+
+@validator(
