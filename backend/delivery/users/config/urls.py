da52ed475bdbfcdf4fd1fac7c84e6defcbe2fc95
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-13 20:09:59 +0100

    Update urls.py

--- backend/delivery/users/config/urls.py
+++ backend/delivery/users/config/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+
 
 urlpatterns = [
   path = ('admin/', admin.site.urls),
@@ -8,9 +8,4 @@ urlpatterns = [
   path = ('users/', include ('users.urls')),
 ]
 
-urlpatterns = [
-  path = ('orders/', views.OrderListView.as_view(), name='order-list'),
-  path = ('orders/'/<int:pk>/', views.OrderDetailView.as_view(), name='order-detail')
-  path = ('track/<str:tracking_code>/', views.TrackOrderView.as_view(), name='track-order'),
-]
 
