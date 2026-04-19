8ff72d33674f98675248346fa3e2276c70a8e015
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-19 18:52:17 +0100

    Update views.py

--- backend/users/migrations/templates/users/views.py
+++ backend/users/migrations/templates/users/views.py
@@ -1,1 +1,21 @@
+from django.views.generic import ListView, DetailView, CreatView
+from .models import Order
+from .forms import OrderFrom
+
+class OrderListView(ListView):
+   model = Order
+   template_name = 'orders/order_list.html
+   context_object_name = 'orders'
+
+class OrderDetailView(DetailView):
+    model = order
+    template_name = 'orders/order_detail.html'
+    
+
+class OrderCreateView(CreateView):
+     model = Order
+     form_class = OrderForm
+     template_form = 'orders/order_form.html'
+     success_url = '/orders/'
+  
 
