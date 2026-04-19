6fd6fdf4c6eef372ab6e358910491dec87467448
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-19 20:24:28 +0100

    Update tests.py

--- backend/users/migrations/templates/users/tests.py
+++ backend/users/migrations/templates/users/tests.py
@@ -1,1 +1,17 @@
+from django.test import TestCase
+from .models import Client, Order
 
+class OrderModelTest(TestCase):
+    def setUp(self):
+        self.client = Client.objects.create(
+         first_name='Berry', last_name='Silva', email='berrysilva145@gmail.com'
+        )
+      self.order = Order.objects.create(
+        client= self.client,
+        delivery_address= '333 Main Str',
+        total_price = 250.00
+      )
+
+    def test_order_creation(self)
+        self.assertEqual(self.order.client.first_name, 'Berry')
+        self.assertEqual(self.order.status, 'pending')
