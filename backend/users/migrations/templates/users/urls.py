eea6fb130995fca93b91a809cc1a713ef2a043d4
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-19 19:07:38 +0100

    Update urls.py

--- backend/users/migrations/templates/users/urls.py
+++ backend/users/migrations/templates/users/urls.py
@@ -1,1 +1,10 @@
+from django.urls  import path
+from . import views
 
+app_name = 'orders'
+
+urlpattern = [
+    path('', views.OrderListView.as_view(), name='order-list'),
+    path('<int:pk>/', views.OrderListView.as_view(), name='order-detail')    
+    path('create/', views.OrderCreateView.as_view(), name='order-create'),
+]
