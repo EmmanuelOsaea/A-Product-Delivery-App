b9ede649ab3d25170ab6a8793e554a05f21be4cb
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-19 17:39:12 +0100

    Update manage.py

--- backend/users/manage.py
+++ backend/users/manage.py
@@ -1,1 +1,19 @@
+#!/usr/bin/env python
 
+import os
+import sys
+
+def main():
+  """Run principal tasks."""
+os.environ.setdefault('DJANGO_SETTINGS_MODULE, 'config.settings')
+try:
+    from django.core.management import execute_from_command_line
+except ImportError as exc:
+    raise ImportError(
+"You couldn't import Django? Have you validated if it's been installed and"
+"existing on your PYTHONPATH environment variable? Did you " "forget to activate a virtual environment
+    ) from exc
+execute_from_command_line(sys.args)
+
+if __name__ == '__main__';
+    main()
