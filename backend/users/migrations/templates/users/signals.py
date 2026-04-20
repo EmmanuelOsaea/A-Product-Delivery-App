0c8e7f07136c7b7210ae9cdf9a1e80f589e8d256
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-20 11:09:29 +0100

    Update signals.py

--- backend/users/migrations/templates/users/signals.py
+++ backend/users/migrations/templates/users/signals.py
@@ -1,1 +1,9 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Order
 
+@receiver(post_save, sender=Order)
+def order_status_changed(sender, instance, created, **kwargs):
+     if not created:
+       # This sends notification when order status changes
+print(f"Order #{instance.id} status changed to {instance.status}") 
