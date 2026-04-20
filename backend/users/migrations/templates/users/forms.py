a4df9e3a60a6eff84c7ec443583519914b52a08a
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-20 11:22:50 +0100

    Update forms.py

--- backend/users/migrations/templates/users/forms.py
+++ backend/users/migrations/templates/users/forms.py
@@ -1,1 +1,10 @@
+from django import forms
+from .models import Order
 
+class OrderFrom(forms.ModelForm)
+    class Meta:
+       model = Order
+        fields = ['client', 'delivery_address', 'notes', 'status']
+       widgets = {
+           'notes': forms.Textarea(attrs={'rows':6});
+      }
