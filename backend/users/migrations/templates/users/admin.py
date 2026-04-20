813ce1ec67b0d2c78d45c3e82fc41b0c3a22646b
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-20 11:44:43 +0100

    Update admin.py

--- backend/users/migrations/templates/users/admin.py
+++ backend/users/migrations/templates/users/admin.py
@@ -1,1 +1,13 @@
+from django.contrib import admin
+from  .models import Order, OrderItem # This import models
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'status', 'order_date', 'total_price')
+    list_filter = ('status', 'order_date')
+    search_fields = ('client_first_name', 'client_last_name', 'id')
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product_name', 'quantity', 'price_per_item')
+    search_field = ('product_name',)
