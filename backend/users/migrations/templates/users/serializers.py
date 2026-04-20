9b28e98cb0afcd3e77ca0cb971daf2f47858159e
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-20 09:53:30 +0100

    Update serializers.py

--- backend/users/migrations/templates/users/serializers.py
+++ backend/users/migrations/templates/users/serializers.py
@@ -1,1 +1,14 @@
+from rest_framework import serializers
+from .models import Order, Client
 
+class ClientSerializer(serializers.ModelSerializer)
+     class Meta:
+         model= Client
+         fields= ['id', 'first_name', 'last_name', 'email', 'phone_number']
+
+  class OrderSerializer(serializers.Modelserializer):
+      client = ClientSerializer(read_only=True)
+
+class Meta:
+  model = Order
+  fields = ['id', 'client', 'order_date', 'status', 'delivery_address', 'note', 'total_price']
