ade8b931cce01183b6a8bec5d0df27b403878703
EmmanuelOsaea <155497725+EmmanuelOsaea@users.noreply.github.com>
2026-04-27 15:17:33 +0100

    Add total_price and delivery_mode to OrderForm fields

--- backend/users/migrations/templates/users/forms.py
+++ backend/users/migrations/templates/users/forms.py
@@ -3,8 +3,10 @@ from .models import Order
 
 class OrderFrom(forms.ModelForm)
     class Meta:
-       model = Order
-        fields = ['client', 'delivery_address', 'notes', 'status']
-       widgets = {
+       model = Order      
+        fields = ['client', 'delivery_address', 'notes', 'status', 'total_price, 'delivery_mode']                 
+        widgets = {
            'notes': forms.Textarea(attrs={'rows':6});
-      }
+           'delivery_mode': forms.Select(attrs={'class': 'form-control'}),
+       }
+
